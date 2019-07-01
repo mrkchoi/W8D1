c17899879bd05175fddd1bb6127516014af7a2d6
Kenneth Choi <kennethichoi@gmail.com>
2019-07-01 14:42:52 -0700

    Implement SessionAPIUtil

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import * as SessionAPIUtil from './utils/session_api_util';
+
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+
+  // testing purposes only
+  window.signUp = SessionAPIUtil.signUp
+  window.logIn = SessionAPIUtil.logIn
+  window.logOut = SessionAPIUtil.logOut
+
+
+
   ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
 });
